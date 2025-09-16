ab1183826d371fd18310372b7ae829058ea3f915
Marijus Ladanovski <electromenn@gmail.com>
2025-09-16 11:58:14 +0300

    Create ImageCard styling

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,19 +1,25 @@
 import { getOptimizedImageUrl } from "../services/picsumApi";
 import type { ImageCardProps } from "../types";
+import "../styles/ImageCard.css";
 
 export const ImageCard = ({ image, width }: ImageCardProps) => {
   const imageUrl = getOptimizedImageUrl(image, width);
 
   if (!imageUrl) {
-    return <div className="image-card-error">Failed to load image</div>;
+    return <div className="image-card__error">Failed to load image</div>;
   }
 
   return (
-    <div>
-      <div>
-        <img src={imageUrl} alt={`Photo by ${image.author}`} loading="lazy" />
-        <div>
-          <span>{image.author}</span>
+    <div className="image-card">
+      <div className="image-card__container">
+        <img
+          src={imageUrl}
+          alt={`Photo by ${image.author}`}
+          className="image-card__image"
+          loading="lazy"
+        />
+        <div className="image-card__author">
+          <span className="image-card__author-text">{image.author}</span>
         </div>
       </div>
     </div>
